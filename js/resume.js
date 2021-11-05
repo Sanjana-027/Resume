faa2901471f9314a4d89b6e48182984174da1355
Sanjana-027 <sanjanasingh02710@gmail.com>
2021-11-05 13:13:19 +0530

    [UPDATE]

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,3 @@
-
 const menuBtn = document.querySelector(".menu-btn");
 const listNav = document.querySelector(".list");
 let menuOpen = false;
@@ -13,10 +12,32 @@ menuBtn.addEventListener("click", () => {
     }
 });
 
-
+// const list = document.querySelector(".navname");
+// list.addEventListener("click", () => {
+//     if (menuOpen) {
+//         closeNav();
+//     }
+// });
 
 function closeNav() {
     menuBtn.classList.remove("open");
     menuOpen = false;
     listNav.classList.remove("open");
 }
+
+// document.querySelector(".navname").addEventListener("click", alert("Hello World!"));
+
+$(document).ready(function () {
+    console.log("called")
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $("#scroll").fadeIn();
+      } else {
+        $("#scroll").fadeOut();
+      }
+    });
+    $("#scroll").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+    });
+  });
